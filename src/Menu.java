e380aec3c0fc163498e3ca760266b28f4f364b8a
Rafael de Lima Tozo <rafaeldelimatozo@gmail.com>
2024-03-20 22:33:42 -0300

    conclusao tarefa2

---
+++ src/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+
+    public void mostrarMenu(Usuario u1){
+        System.out.println(u1.getUsername());
+        System.out.println(u1.getIdade());
+        System.out.println(u1.getEmail());
+        System.out.println(u1.getSenha);
+        System.out.println(u1.getVerificado);
+    }
+
+}
